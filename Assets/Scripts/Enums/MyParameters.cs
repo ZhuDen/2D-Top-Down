28a6817be095b6321dbc3bb0bdecb814ef989afa
Илья Алешин <74253695+MamkaTrap@users.noreply.github.com>
2023-10-31 23:56:25 +0300

    MyParameters

---
+++ Assets/Scripts/Enums/MyParameters.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+    [DataContract]
+    public enum MyParameters : byte
+    {
+        KeyName,
+    }
+
--- Server/GameLibrary/Common/ParameterCode.cs
+++ Server/GameLibrary/Common/ParameterCode.cs
@@ -31,5 +31,6 @@ namespace GameLibrary.Common
 
         TeamUUID,
         TeamMember,
+        KeyName,
     }
 }
